346b95ef168e3f85ccdad180afe082a4fc8c0a29
Guilherme Saturnino <guilherme.bicalho2@gmail.com>
2024-10-07 09:27:20 +0200

    day21, pt2

--- src/day21.cpp
+++ src/day21.cpp
@@ -75,12 +75,87 @@ void solve_pt1()
         std::cout << string << std::endl;
     }
 }
+
+void solve_pt2()
+{
+    std::fstream file("inputs/day21");
+    std::string line;
+    static std::regex swap_pos("swap position (\\d+) with position (\\d+)");
+    static std::regex swap_letter("swap letter (\\w) with letter (\\w)");
+    static std::regex rotate_steps("rotate (\\w+) (\\d+) steps?");
+    static std::regex rotate_pos("rotate based on position of letter (\\w+)");
+    static std::regex reverse_pos("reverse positions (\\d+) through (\\d+)");
+    static std::regex move_pos("move position (\\d+) to position (\\d+)");
+    std::smatch sm;
+    std::string string("fbgdceah");
+    //std::string string("cbeghdaf");
+    //std::string string("decab");
+    std::vector<std::string> lines;
+    while (getline(file, line))
+    {
+        lines.emplace_back(line);
+    }
+    std::reverse(lines.begin(), lines.end());
+    for (auto &line: lines)
+    {
+        if (std::regex_match(line, sm, swap_pos))
+        {
+            auto pos1 = std::stoi(sm[1]);
+            auto pos2 = std::stoi(sm[2]);
+            std::swap(string[pos1], string[pos2]);
+
+        } else if (std::regex_match(line, sm, swap_letter))
+        {
+            auto l1 = sm[1].str()[0];
+            auto l2 = sm[2].str()[0];
+            std::replace(string.begin(), string.end(), l1, '.');
+            std::replace(string.begin(), string.end(), l2, l1);
+            std::replace(string.begin(), string.end(), '.', l2);
+        } else if (std::regex_match(line, sm, rotate_steps))
+        {
+            auto sign = (sm[1] == "left")? -1 : 1;
+            auto nsteps = sign * std::stoi(sm[2]);
+            string = rotate_str(string, nsteps);
+        } else if (std::regex_match(line, sm, rotate_pos))
+        {
+            auto l = sm[1].str()[0];
+            // Y = (2X + 1) % N
+            // if y < X => (2X + 1) = Y + N => X = (Y + N - 1) / 2, x < 4
+            // if y < X => (2X + 2) = Y + N => X = (Y + N - 2) / 2, x >= 4
+            int pos_after = std::find(string.begin(), string.end(), l) - string.begin();
+            int pos_before = 0;
+            while (pos_after != (2 * pos_before + 1 + (pos_before >= 4))%string.length())
+            {
+                pos_before++;
+            }
+            std::cout << pos_after << ", " << pos_before << std::endl;
+            string = rotate_str(string, pos_after - pos_before);
+        } else if (std::regex_match(line, sm, reverse_pos))
+        {
+            auto pos1 = std::stoi(sm[1]);
+            auto pos2 = std::stoi(sm[2]);
+            std::reverse(string.begin()+pos1, string.begin()+pos2+1);
+        } else if (std::regex_match(line, sm, move_pos))
+        {
+            auto pos1 = std::stoi(sm[1]);
+            auto pos2 = std::stoi(sm[2]);
+            auto c = string[pos2];
+            string.erase(pos2, 1);
+            string.insert(pos1, 1,  c);
+        } else
+        {
+            std::cout << line << std::endl;
+        } 
+        std::cout << line << std::endl;
+        std::cout << string << std::endl;
+    }
+}
  
 int main()
 {
     auto started = std::chrono::high_resolution_clock::now();
-    solve_pt1();
-   //solve_pt2();
+    //solve_pt1();
+    solve_pt2();
     auto done = std::chrono::high_resolution_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(done-started).count() << "ms\n";
 
